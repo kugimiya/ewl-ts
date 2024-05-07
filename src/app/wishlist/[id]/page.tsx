370556e74fb121045c4be62e540d9480adfef2a1
kugimiya <bp.001@yandex.ru>
2024-05-08 01:57:25 +0400

    tune

--- src/app/wishlist/[id]/page.tsx
+++ src/app/wishlist/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import { getDb } from "@/utils/getDb";
 import { WishlistPageComponent } from "@/app/wishlist/[id]/WishlistPageComponent";
+import Link from "next/link";
 
 export default async function WL({ params }: { params?: { id: string } }) {
   const prisma = getDb();
@@ -12,6 +13,10 @@ export default async function WL({ params }: { params?: { id: string } }) {
   return (
     <div>
       <h1>список_желание пользователя {user.nickname}</h1>
+      <br />
+      <p>Можете просто нажать на название подарка, и он будет занят вами. Это анонимно и не требует регистрации. Больше не будет ситуации, когда человеку подарят два одинаковых подарка. :)</p>
+      <br />
+      <p>Тоже захотел такой вишлист? <Link href='/register'>Пройди регистрацию и заведи</Link></p>
       <br />
       <WishlistPageComponent rawWishlist={user.wishlist} nickname={params?.id || ''} />
     </div>
